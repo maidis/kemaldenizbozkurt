3e4af201f2d04a670e72b013815aa4abebe143aa
Anıl Özbek <ozbekanil@gmail.com>
2023-10-18 15:35:48 +0300

    .
    .

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 
 let header = $(`
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="navbar">
-<a class="navbar-brand" href="index.html">Rohan Modi </a>
+<a class="navbar-brand" href="index.html">Kemal Deniz Bozkurt </a>
 <div class="hamburger_wrapper navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 
   <div id="js-hamburger" class="hamburger">
